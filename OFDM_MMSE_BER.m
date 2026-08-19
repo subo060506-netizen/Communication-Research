aa3af90d4dbd1865b1d97c6b1e02a22383d7c4a0
subo060506-netizen <subo060506@gmail.com>
2026-08-19 10:48:47 +0800

    OFDM system simulation with MMSE and ZF equalization, comparing BER performance under different SNR conditions.

--- OFDM_MMSE_BER.m
+++ OFDM_MMSE_BER.m
@@ -33,33 +33,33 @@ qamPower = mean(abs(qamModOut(:)).^2);
 MSE_ZF = zeros(length(SNR),1);
 MSE_MMSE = zeros(length(SNR),1);
 for i=1:length(SNR)
-snr=SNR(i);
-%chanOut=awgn(mpChanOut,snr,"measured");
-%awgn中SNR信号功率是mpChanOut信号，与MMSE中不同
-% Calculate noise power
-snrLinear = 10^(snr/10);
-noiseVarFreq = qamPower / snrLinear;
-noiseVarTime = noiseVarFreq / numCarr;
-%Generate complex AWGN
-noise = sqrt(noiseVarTime/2) * (randn(size(mpChanOut)) + 1j*randn(size(mpChanOut)));%randn(size(mpChanOut))：生成与mpChanOut维度相同的实高斯噪声（均值=0，方差=1）
-%σ² = noiseVarTime/2 才能使总噪声功率等于 noiseVarTime
-chanOut = mpChanOut + noise;
-rx = reshape(chanOut,numCarr+cycPrefLen,[]);
-rxNoCP = rx(cycPrefLen+1:end,:);
-fftModOut=fft(rxNoCP,numCarr,1);
-mpChanFreq=fft(mpChan,numCarr);
-noiseVarRatio = 1/snrLinear;
-mmseWeight = conj(mpChanFreq) ./ (abs(mpChanFreq).^2 + noiseVarRatio);
-eqOut_ZF=fftModOut ./mpChanFreq;
-eqOut_MMSE= fftModOut .* mmseWeight;
-%MMSE估计的幅度收缩α[k]=∣H[k]∣^2/(|H[k]|^2​+λ)→QAM判决偏差
-%理解：MMSE允许一定的信号衰减,换取减少噪声增强
-qamdeModOut_ZF=qamdemod(eqOut_ZF,modOrder,OutputType="bit");
-qamdeModOut_MMSE=qamdemod(eqOut_MMSE,modOrder,OutputType="bit");
-BER_ZF(i)=nnz(qamdeModOut_ZF(:)~=srcBits)/numBits;
-BER_MMSE(i)=nnz(qamdeModOut_MMSE(:)~=srcBits)/numBits;
-MSE_ZF(i) = mean(abs(eqOut_ZF(:) - qamModOut(:)).^2);
-MSE_MMSE(i) = mean(abs(eqOut_MMSE(:) - qamModOut(:)).^2);
+    snr=SNR(i);
+    %chanOut=awgn(mpChanOut,snr,"measured");
+    %awgn中SNR信号功率是mpChanOut信号，与MMSE中不同
+    % Calculate noise power
+    snrLinear = 10^(snr/10);
+    noiseVarFreq = qamPower / snrLinear;
+    noiseVarTime = noiseVarFreq / numCarr;
+    %Generate complex AWGN
+    noise = sqrt(noiseVarTime/2) * (randn(size(mpChanOut)) + 1j*randn(size(mpChanOut)));%randn(size(mpChanOut))：生成与mpChanOut维度相同的实高斯噪声（均值=0，方差=1）
+    %σ² = noiseVarTime/2 才能使总噪声功率等于 noiseVarTime
+    chanOut = mpChanOut + noise;
+    rx = reshape(chanOut,numCarr+cycPrefLen,[]);
+    rxNoCP = rx(cycPrefLen+1:end,:);
+    fftModOut=fft(rxNoCP,numCarr,1);
+    mpChanFreq=fft(mpChan,numCarr);
+    noiseVarRatio = 1/snrLinear;
+    mmseWeight = conj(mpChanFreq) ./ (abs(mpChanFreq).^2 + noiseVarRatio);
+    eqOut_ZF=fftModOut ./mpChanFreq;
+    eqOut_MMSE= fftModOut .* mmseWeight;
+    %MMSE估计的幅度收缩α[k]=∣H[k]∣^2/(|H[k]|^2​+λ)→QAM判决偏差
+    %理解：MMSE允许一定的信号衰减,换取减少噪声增强
+    qamdeModOut_ZF=qamdemod(eqOut_ZF,modOrder,OutputType="bit");
+    qamdeModOut_MMSE=qamdemod(eqOut_MMSE,modOrder,OutputType="bit");
+    BER_ZF(i)=nnz(qamdeModOut_ZF(:)~=srcBits)/numBits;
+    BER_MMSE(i)=nnz(qamdeModOut_MMSE(:)~=srcBits)/numBits;
+    MSE_ZF(i) = mean(abs(eqOut_ZF(:) - qamModOut(:)).^2);
+    MSE_MMSE(i) = mean(abs(eqOut_MMSE(:) - qamModOut(:)).^2);
 end
 hold on
 semilogy(SNR,BER_ZF,"o-r")%在 x 轴上使用线性刻度、在 y 轴上使用以 10 为底的对数刻度来绘制 x 和 y 坐标
